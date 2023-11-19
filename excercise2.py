abb874a9e61f2c402db00d610c1caf4ed7dee772
yinyinghao <yinyinghaoyinyinghao@gmail.com>
2023-11-19 23:00:34 +0800

    11.19

--- excercise2.py
+++ excercise2.py
@@ -79,5 +79,204 @@ else:
             print('Students:', Student.count)
             print('测试通过!')
 
+#使用__slots__
+#我们通过__slots__变量来限制class实例能添加的属性，也就是这个变量内的是只能添加的属性
+class Student(object):
+    __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
 
+#使用__slots__要注意，__slots__定义的属性仅对当前类实例起作用，对继承的子类是不起作用的：
+class GraduateStudent(Student):
+    pass
+g = GraduateStudent()
+g.score = 9999
+#除非在子类中也定义__slots__，这样，子类实例允许定义的属性就是自身的__slots__加上父类的__slots__。
+
+class Student(object):
+    def get_score(self):
+        return self.__score
+    def set_score(self,value):
+        if not isinstance(value,int):
+            raise ValueError('score must be an integer')
+        if value <0 or value >100:
+            raise ValueError('score must between 0 and 100')
+        else:
+            self.__score = value
+
+#decorator可以为函数动态加上功能，
+# python中内置的@property装饰器就是把一个方法变成属性调用
+class Student(object):
+
+    @property
+    def score(self):
+        return self.score
+    
+    @score.setter
+    def score(self,value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+           
+#请利用@property给一个Screen对象加上width和height属性，
+# 以及一个只读属性resolution：
+class Screen(object):
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self,value1):
+        self.__width = value1
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self,value2):
+        self.__height = value2
+
+    @property
+    def resolution(self):
+        return self.__width*self.__height
+    
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
+
+#多重继承
+# 由于Python允许使用多重继承，因此，MixIn就是一种常见的设计。
+#只允许单一继承的语言（如Java）不能使用MixIn的设计。
+
+#定制类
+#len()是函数，__len__()是方法，如果没有对__len__()的返回值自定义，则返回输入参数的长度
+#__slots__是定义实例能够使用的变量。
+
+#直接显示变量调用的不是__str__()，而是__repr__()，
+# 两者的区别是__str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，
+# 也就是说，__repr__()是为调试服务的。
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return 'Student object (name=%s)' % self.name
+    __repr__ = __str__ #在正常情况下我们是在调用实例时时直接返回<__main__.Student object at 0x109afb190>，但在这里我们把__str__赋给__repr__
+
+s = Student('Michael')
+print(s)
+
+#__iter()__
+#如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，该方法返回一个迭代对象，
+#然后，Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，
+#直到遇到StopIteration错误时退出循环。
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self # 实例本身就是迭代对象，故返回自己
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+        if self.a > 100000: # 退出循环的条件
+            raise StopIteration()
+        return self.a # 返回下一个值
+for n in Fib():
+    print(n)
+
+#这里的__next__()是一个生成器。也是惰性的。要通过取数激活。
+#还要强调一下yield
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
+'''
+starting...
+4
+********************
+res: None
+4
+'''
+'''
+1.程序开始执行以后，因为foo函数中有yield关键字，所以foo函数并不会真的执行，而是先得到一个生成器g(相当于一个对象)
+
+2.直到调用next方法，foo函数正式开始执行，先执行foo函数中的print方法，然后进入while循环
+
+3.程序遇到yield关键字，然后把yield想想成return,return了一个4之后，程序停止，并没有执行赋值给res操作，此时next(g)语句执行完成，所以输出的前两行
+（第一个是while上面的print的结果,第二个是return出的结果）是执行print(next(g))的结果，
+
+4.程序执行print("*"*20)，输出20个*
+
+5.又开始执行下面的print(next(g)),这个时候和上面那个差不多，不过不同的是，这个时候是从刚才那个next程序停止的地方开始执行的，也就是要执行res的赋值操作，这时候要注意，
+这个时候赋值操作的右边是没有值的（因为刚才那个是return出去了，并没有给赋值操作的左边传参数），所以这个时候res赋值是None,所以接着下面的输出就是res:None,
+
+6.程序会继续在while里执行，又一次碰到yield,这个时候同样return 出4，然后程序停止，print函数输出的4就是这次return出的4.
+'''
+
+#Fib()是无法直接访问的，他只是一个存储器，我们需要去调取，而我们需要像list去取出元素。
+class Fib(object):
+    def __getitem__(self,n):
+        a,b =1,1
+        for x in range(n):
+            a,b = b,a+b
+        return a
+f = Fib()
+print(f[10])
+
+#__getitem__()传入的参数可能是一个int，也可能是一个切片对象slice
+class Fib(object):
+    def __getitem__(self, n):
+        if isinstance(n, int): # n是索引
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice): # n是切片
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+        
+#如果把对象看成dict,__getitem__()的参数也可能是一个可以作key的object，例如str
+#__getattr__
+class Student(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr=='score':
+            return 99
+#注意，只有在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name，不会在__getattr__中查找。
+
+#这段代码要好好看一下，他的进程是怎么样的，其中__str__后半部分前面有提到。
+class Chain(object):
+
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
 
